7d01e4064350a61f36f798f17df90ec351a91fae
Robert Brunson <robert.brunson@superleague.com>
2021-11-01 17:10:46 -0400

    maven

---
+++ src/main/java/moe/quill/feather/Feather.kt
@@ -0,0 +1,14 @@
+package moe.quill.feather;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Feather : JavaPlugin() {
+
+    override fun onEnable() {
+        super.onEnable()
+    }
+
+    override fun onDisable() {
+        super.onDisable()
+    }
+}
