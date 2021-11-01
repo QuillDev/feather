fb3cb999ab43b89b4b5b7d3ae25a480cd2a697d1
Robert Brunson <robert.brunson@superleague.com>
2021-11-01 16:37:03 -0400

    added maven publish

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,8 +3,9 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     id("java")
-    kotlin("jvm") version "1.5.31"
     id("com.github.johnrengelman.shadow") version "7.1.0"
+    id("maven-publish")
+    kotlin("jvm") version "1.5.31"
 }
 
 group = "moe.quill"
