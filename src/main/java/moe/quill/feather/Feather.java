475d11050a9c7770a50dac82cb345a8418ebdb64
Robert Brunson <robert.brunson@superleague.com>
2021-11-01 17:26:05 -0400

    gradle2

---
+++ src/main/java/moe/quill/feather/Feather.java
@@ -0,0 +1,17 @@
+package moe.quill.feather;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Feather extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
